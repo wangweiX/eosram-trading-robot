fa3e6593bf4912777ad165408bf568893146d622
wangwei <w7years@gmail.com>
2018-07-11 19:06:23 +0800

    refactor app.js code

--- app.js
+++ app.js
@@ -26,99 +26,107 @@ shell.exec(pkill_keosd);
 setInterval(
     function () {
         async.parallel(
-            getAccountData(),
-            function (err, results) {
-                dealResult(results);
+            tasks(), function (err, results) {
+                dealResults(results);
             });
     },
     config.interval
 );
 
 /**
- * Query eos account data
+ * Query data
  * @returns {{account: account, quoteBalance: quoteBalance, available: available}}
  */
-function getAccountData() {
+function tasks() {
     return {
-        /**
-         * Query my eos account data
-         * @param callback
-         */
         account: function (callback) {
-            eosClient.getAccount({
-                json: "true", code: "eosio", "account_name": config.wallet.account_name
-            }).then((data) => {
-                let my_eos = data.core_liquid_balance;
-                let my_eos_amount = parseFloat(my_eos.split(" ")[0]);
-                let my_quota_ram = data.ram_quota;
-                callback(null, {
-                    my_eos_amount: my_eos_amount,
-                    my_quota_ram: my_quota_ram
-                });
-            }).catch((e) => {
-                console.error(e);
-            })
+            getMyAccountData(callback);
         },
-
-        /**
-         * Check eosio.ram account data
-         * @see https://eosflare.io/account/eosio.ram
-         * @param callback
-         */
         quoteBalance: function (callback) {
-            eosClient.getTableRows({
-                json: "true", code: "eosio", scope: "eosio", table: "rammarket", limit: "10"
-            }).then((data) => {
-                let obj = data.rows[0];
-                let quote_balance = obj.quote.balance;
-                callback(null, quote_balance);
-            }).catch((e) => {
-                console.error(e);
-            })
+            quoteBalance(callback);
         },
-
-        /**
-         * Query eosio.ram memory data
-         * @see https://eosflare.io/account/eosio.ram
-         * @param callback
-         */
         available: function (callback) {
-            eosClient.getTableRows({
-                json: "true", code: "eosio", scope: "eosio", table: "global"
-            }).then((data) => {
-                let obj = data.rows[0];
-                let max_ram_size = obj.max_ram_size;
-                let reserved = obj.total_ram_bytes_reserved;
-                let available = (max_ram_size - reserved) / 1024; //in kb
-                let percent = reserved / max_ram_size;
-                callback(null, {
-                    available: available,
-                    reserved: reserved / 1024,
-                    percent: percent
-                });
-            }).catch((e) => {
-                console.error(e);
-            })
+            getRamMarketData(callback);
         }
     };
+}
+
+/**
+ * Query my eos account data
+ * @param callback
+ */
+function getMyAccountData(callback) {
+    eosClient.getAccount({
+        json: "true", code: "eosio", "account_name": config.wallet.account_name
+    }).then((data) => {
+        let my_eos = data.core_liquid_balance;
+        let my_eos_amount = parseFloat(my_eos.split(" ")[0]);
+        let my_quota_ram = data.ram_quota;
+        callback(null, {
+            my_eos_amount: my_eos_amount,
+            my_quota_ram: my_quota_ram
+        });
+    }).catch((e) => {
+        console.error(e);
+    })
+}
+
+/**
+ * Query eosio.ram account data
+ * @see https://eosflare.io/account/eosio.ram
+ * @param callback
+ */
+function quoteBalance(callback) {
+    eosClient.getTableRows({
+        json: "true", code: "eosio", scope: "eosio", table: "rammarket", limit: "10"
+    }).then((data) => {
+        let obj = data.rows[0];
+        let quote_balance = obj.quote.balance;
+        let quote_balance_amount = parseFloat(quote_balance.split(" ")[0]);
+        callback(null, {
+            quote_balance_amount: quote_balance_amount
+        });
+    }).catch((e) => {
+        console.error(e);
+    })
+}
+
+/**
+ * Query Ram Market Data
+ * @see https://eosflare.io/account/eosio.ram
+ * @param callback
+ */
+function getRamMarketData(callback) {
+    eosClient.getTableRows({
+        json: "true", code: "eosio", scope: "eosio", table: "global"
+    }).then((data) => {
+        let obj = data.rows[0];
+        let max_ram_size = obj.max_ram_size;
+        let reserved_ram = obj.total_ram_bytes_reserved;
+        let available_ram = (max_ram_size - reserved_ram) / 1024; //in kb
+        let percent = reserved_ram / max_ram_size;
+        callback(null, {
+            available_ram: available_ram,
+            percent: percent
+        });
+    }).catch((e) => {
+        console.error(e);
+    })
 }
 
 /**
  *
  * @param results
  */
-function dealResult(results) {
-    let available = results.available.available;
+function dealResults(results) {
     let percent = results.available.percent;
-    let quote_balance = results.quoteBalance;
-    let quote_balance_amount = parseFloat(quote_balance.split(" ")[0]);
-    let price = quote_balance_amount / available;
+    let price = results.quoteBalance.quote_balance_amount / results.available.available_ram;
     let my_eos_amount = results.account.my_eos_amount;
     let my_quota_ram = results.account.my_quota_ram;
 
     logger.info("Current RAM Price:", price, "EOS/KB"
-        , ", eosio.ram total balance:", quote_balance_amount - 1000000, "EOS"
         , ", percent:", percent * 100
+        , ", eosio.ram total EOS amount:", (results.quoteBalance.quote_balance_amount - 1000000), "EOS"
         , ", buy_line:", config.buy_line, "EOS/KB"
         , ", sell_line:", config.sell_line, "EOS/KB"
         , ", my_eos_amount:", my_eos_amount, "EOS"
@@ -136,8 +144,9 @@ function dealResult(results) {
     }
 }
 
+
 /**
- * purchase eos ram
+ * buy eos ram
  * @param price
  * @param my_eos_amount
  * @param buy_msg
